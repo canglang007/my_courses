a9a6317a3d8b742cd540fe6025ec9c37b923b306
Zhuoming Liu <2097254505@qq.com>
2024-04-02 08:23:27 +0800

    增加了注释

--- 医学信息处理/期末任务_肝脏图像分割/seg/code/main/unet/unet_parts.py
+++ 医学信息处理/期末任务_肝脏图像分割/seg/code/main/unet/unet_parts.py
@@ -50,6 +50,7 @@ class Up(nn.Module):
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
+            # 利用反卷积来上采样
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
 
